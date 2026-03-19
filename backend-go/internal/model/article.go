91ed4b4ba7813588c235e9930ccbb24073753677
HandyWote <18666119673@163.com>
2026-03-19 20:49:07 +0800

    refactor(model): 为 JSONArray 实现 sql.Scanner 和 driver.Valuer 接口，支持数据库 JSONB 字段存取

--- backend-go/internal/model/article.go
+++ backend-go/internal/model/article.go
@@ -1,6 +1,9 @@
 package model
 
 import (
+	"database/sql/driver"
+	"encoding/json"
+	"fmt"
 	"time"
 )
 
@@ -18,3 +21,36 @@ type Article struct {
 }
 
 type JSONArray []map[string]string
+
+// Scan 实现 sql.Scanner 接口，从数据库读取 JSONB 字段
+func (j *JSONArray) Scan(value interface{}) error {
+	if value == nil {
+		*j = nil
+		return nil
+	}
+
+	var data []byte
+	switch v := value.(type) {
+	case []byte:
+		data = v
+	case string:
+		data = []byte(v)
+	default:
+		return fmt.Errorf("cannot scan %T into JSONArray", value)
+	}
+
+	if len(data) == 0 {
+		*j = nil
+		return nil
+	}
+
+	return json.Unmarshal(data, j)
+}
+
+// Value 实现 driver.Valuer 接口，用于写入数据库
+func (j JSONArray) Value() (driver.Value, error) {
+	if j == nil {
+		return "[]", nil
+	}
+	return json.Marshal(j)
+}
