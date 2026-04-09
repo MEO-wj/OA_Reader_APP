bd6a73e3cbe82de87be609e1ffe47a1a8841c559
HandyWote <18666119673@163.com>
2026-04-09 13:51:23 +0800

    fix: update shell scripts to use absolute python paths

--- crawler/backfill.sh
+++ crawler/backfill.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-cd /vol1/1000/programs/OA-Reader/crawler/
-source .venv/bin/activate
-uv run backfill.py
+
+/vol1/1000/programs/OA-Reader/crawler/.venv/bin/python3 /vol1/1000/programs/OA-Reader/crawler/backfill.py
--- crawler/run.sh
+++ crawler/run.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-
-cd /vol1/1000/programs/OA-Reader/crawler/
-source .venv/bin/activate
-uv run main.py
+/vol1/1000/programs/OA-Reader/crawler/.venv/bin/python3 /vol1/1000/programs/OA-Reader/crawler/main.py
